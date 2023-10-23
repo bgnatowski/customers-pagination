91dd3da735d6ca3812ca20ad0b3822593f4ec4c2
bgnatowski <bgnatowski00@gmail.com>
2023-10-23 16:08:07 +0200

    changes to fit the study project; dockerized

--- src/main/java/pl/bgnat/pagination/service/DataInitializer.java
+++ src/main/java/pl/bgnat/pagination/service/DataInitializer.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import pl.bgnat.pagination.domain.Customer;
@@ -20,12 +21,14 @@ import java.util.Arrays;
 @Slf4j
 public class DataInitializer {
 	private final CustomerRepository customerRepository;
+	@Value("${data.file.path}") // lub "${data.file.path.production}" dla profilu "production"
+	private String dataFilePath;
 	@Transactional
 	@PostConstruct
 	public void initializeData() {
 		try {
 			if(!customerRepository.existsById(1L)){
-				String data = new String(Files.readAllBytes(Paths.get("src/main/resources/data.txt")));
+				String data = new String(Files.readAllBytes(Paths.get(dataFilePath)));
 				ObjectMapper objectMapper = new ObjectMapper();
 				objectMapper.registerModule(new JavaTimeModule());
 				Customer[] employees = objectMapper.readValue(data, Customer[].class);
